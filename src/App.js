c920174b3e5a4fd650e8fbe8699da370c4b1e45f
Agustin Di Carlo <agus.dicarlo@gmail.com>
2024-01-16 12:32:25 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ringa from './ringa.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={ringa} className="App-logo mb-4 border rounded-full border-none" alt="logo" />
+        <p className='text-3xl text-blue-600'>
+          Landing gero re bonete.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="text-white italic bg-slate-500 mt-8 p-4 border rounded-lg hover:bg-blue-800 hover:rotate-12 hover:transition"
+          href="https://www.gimnasia.org.ar/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dale lobo
         </a>
       </header>
     </div>
